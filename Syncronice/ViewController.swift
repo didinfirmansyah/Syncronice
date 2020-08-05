1519e80d1d8661b550f6749fc92803e2f09b268b
Didin <dfirmansyah058@gmail.com>
2020-08-05 15:03:08 +0700

    Initial Commit

---
+++ Syncronice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Syncronice
+//
+//  Created by Didin Firmansyah on 05/08/20.
+//  Copyright © 2020 Didin Firmansyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
