463111f990d8fc812eaa0345fd06ac49b1d4ece5
a11-one <a11_one@yahoo.com>
2020-08-10 22:06:11 +0700

    Initial Commit for Syncronice Project

---
+++ Syncronice/Controller/MainController.swift
@@ -0,0 +1,21 @@
+//
+//  MainController.swift
+//  Syncronice
+//
+//  Created by Aldowan Apta Premala on 10/08/20.
+//  Copyright © 2020 Didin Firmansyah. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.init(rgb: 0xFAFFF0)
+        
+    }
+    
+
+}
