1998d80a44f475ec1b536198a146bf7da7966a13
a11-one <a11_one@yahoo.com>
2020-08-12 22:03:34 +0700

    Copy punya didin ke punya aldo

--- Syncronice/SceneDelegate.swift
+++ Syncronice/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = HomeController()
+        self.window?.rootViewController = OnBoardController()
         self.window?.makeKeyAndVisible()
     }
 
